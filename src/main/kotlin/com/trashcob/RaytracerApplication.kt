23508cafbb348dcc564e79e11ae887f2f2f7d613
Travis McMahon <azgoalie@gmail.com>
2017-06-08 00:20:12 -0700

    Removed new lines

--- src/main/kotlin/com/trashcob/RaytracerApplication.kt
+++ src/main/kotlin/com/trashcob/RaytracerApplication.kt
@@ -3,8 +3,6 @@ package com.trashcob
 import javafx.application.Application
 import tornadofx.App
 
-
-
 class RaytracerApp : App(RaytracedImage::class)
 
 fun main(args: Array<String>) = Application.launch(RaytracerApp::class.java, *args)
